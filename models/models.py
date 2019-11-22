b6cdc6c02c974bfbce8ae640b3c1a48adff80734
odoo12 <anto12duran>
2019-11-21 19:26:48 -0500

    relaciones en proceso

--- models/models.py
+++ models/models.py
@@ -5,20 +5,21 @@ from odoo import models, fields
 
 class ReporteTarea(models.Model):
     _name = "reporte.tarea"  # tabla de bd => reporte_tarea
-    _description = "Reporte Tarea"
+    # _description = "Reporte Tarea"
 
     name = fields.Char(string="Tarea", required=True, )
     date_form = fields.Datetime(string="Starting Date", default=fields.Datetime.now(), )
     date_to = fields.Datetime(string="End Date", required=False, )
 
     lote_id = fields.Many2one(comodel_name="reporte.lote", string="lote", required=True, )
+    actividad_ids = fields.One2many('reporte.actividad', inverse_name='reportetarea_id', string="Actividades", )
 
 
-#    formulacion_ids = fields.One2many('reporte.formulacion', inverse_name='reportetarea_id', string='reporte.formulacion', )
+#    formulacion_ids = fields.One2many('reporte.formulacion', inverse_name='reportetarea_id', string='Formulacion', )
 
 class lote(models.Model):
     _name = "reporte.lote"  # table de bd => reporte_lote
-    _description = "Lotes"
+    # _description = "Lotes"
 
     name = fields.Char(string="Lote", required=True)
     description = fields.Char(string="Description", required=True)
@@ -27,38 +28,38 @@ class lote(models.Model):
 
 class responsable(models.Model):
     _name = "reporte.responsable"  # tabla de bd => reporte_responsable
-    _description = "Responsable"
+    #_description = "Responsable"
 
     name = fields.Char(string="Responsable", required=True, )
     description = fields.Char(string="Description", required=True, )
     employee_id = fields.Many2one(comodel_name="hr.employee", string="empleado", required=True, )
-    # actividad_id =fields.Many2one(comodel_name="reporte.actividad", string="actividad", required=True, )
+    actividad_ids = fields.One2many('reporte.actividad', inverse_name='responsable_id', string='Actividad')
 
 
 class formulacion(models.Model):
     _name = "reporte.formulacion"  # tabla de bd => reporte_formulacion
-    _description = "Formulacion"
+    #_description = "Formulacion"
 
     name = fields.Char(string="Formulacion", required=True)
     description = fields.Char(string="Description", required=True, )
     # reportetarea_id = fields.Many2one(comodel_name="reporte_tarea", string="tarea", required=True, )
-    # product_id = fields.Many2one(comodel_name="product.product", string="producto", required=True, )
+    product_id = fields.Many2one(comodel_name="product.product", string="producto", required=True, )
 
 
 class TipoActividad(models.Model):
     _name = "reporte.tipoactividad"  # tabla de bd => reporte_tipoactividad
-    _description = "TipoActividad"
+    # _description = "TipoActividad"
 
-    name = fields.Char(string="Tipo Actividad", required=True)
-    description = fields.Char(string="description", required=True)
-    actividad_ids = fields.One2many('reporte.actividad', inverse_name='tipoactividad_id', string='Actividad')
-
+    name = fields.Char(string="Tipo Actividad", )
+    description = fields.Char(string="description", )
+    actividad_ids = fields.One2many('reporte.actividad', inverse_name='tipoactividad_id', string='Tarea/actividades')
 
 class Actividad(models.Model):
     _name = "reporte.actividad"
-    _description = "Actividad"
+    #_description = "Actividad"
 
     name = fields.Char(string="Actividad", required=True)
-    description = fields.Char(string="description", required=True)
+    #description = fields.Char(string="description", required=True)
     tipoactividad_id = fields.Many2one(comodel_name="reporte.tipoactividad", string="Tipo Actividad", required=True, )
-    #responsable_ids = fields.One2many('reporte.responsable', inverse_name='actividad_id', string='reporte.responsable')
+    responsable_id = fields.Many2one(comodel_name="reporte.responsable", string="Responsable", )
+    reportetarea_id = fields.Many2one(comodel_name="reporte.tarea", string="Tarea" )
